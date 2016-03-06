a495e19d3bd2c9c2358371f9454f30d4b0ccd99b
AlesH <ales@outlook.com>
2016-03-05 18:04:06 -0800

    changing to Azure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 var port = process.env.PORT || 8080;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hello Azure\n');
 }).listen(port);
 
 console.log("listening on port " + port);
