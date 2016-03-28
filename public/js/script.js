dbcde351df0e7bdc212058d77338936805b0113b
AlesH <ales@outlook.com>
2016-03-27 18:01:52 -0700

    Adding Event listeners into

--- public/js/script.js
+++ public/js/script.js
@@ -17,10 +17,15 @@ function getData() {
     });
 }
 
+function selectDateFunc(eventInfo) {
+    console.log(this.textContent);
+}
+
 WinJS.Namespace.define("Sample.ListView", {
     generate: generateItems,
     data: new WinJS.Binding.List(itemArray),
     itemCount: itemArray.length,
+    selectDate: selectDateFunc,
     eh: {
         footerVisibility: WinJS.UI.eventHandler(function (ev) {
             // var visible = ev.detail.visible;
@@ -41,8 +46,18 @@ WinJS.UI.processAll().done(function () {
         Sample.ListView.counter = document.querySelector("#counter");
         Sample.ListView.status = document.querySelector(".footer .status");
         Sample.ListView.progress = document.querySelector(".footer .progress");
-         Sample.ListView.footer = document.querySelector(".footer");
+        Sample.ListView.footer = document.querySelector(".footer");
+
         var splitView = document.querySelector(".splitView").winControl;
+        splitView.openedDisplayMode = WinJS.UI.SplitView.OpenedDisplayMode.overlay; 
+        splitView.closedDisplayMode = WinJS.UI.SplitView.ClosedDisplayMode.none;
+        
+        var commands = document.querySelector(".nav-commands").children;
+        var len = commands.length;
+        for (var i = 0; i< len; i++) {
+            commands[i].winControl.addEventListener("invoked", selectDateFunc);
+        }
+        
         new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
         WinJS.Binding.processAll(Sample.ListView.counter, Sample.ListView).then(function() {
            getData();
