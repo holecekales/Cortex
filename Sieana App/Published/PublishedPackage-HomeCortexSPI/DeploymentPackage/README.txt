7122bfbec888715c09bb505a4c26e0e41968e2b4
alesh <ales@outlook.com>
2017-09-09 18:45:15 -0700

    Deleting the Siena App

--- Sieana App/Published/PublishedPackage-HomeCortexSPI/DeploymentPackage/README.txt
+++
@@ -1,6 +0,0 @@
-﻿TO INSTALL YOUR SIENA APP:
- 
-1. Right-click Add_AppxPackage.ps1
-2. Select "Run with PowerShell"
- 
-For more information, please see Project Siena release documentation.
--- Sieana App/Published/PublishedPackage-HomeCortexSPI/HomeCortexSPI/controls/common/dialog/waitDialogBody.js
+++
@@ -1,9 +0,0 @@
-﻿//!
-//! Copyright (C) Microsoft Corporation.  All rights reserved.
-//!
-(function() {"use strict";
-    var WaitDialogBodyView = WinJS.Class.define(function WaitDialogBodyView_ctor(element) {
-            ko.applyBindings(element.viewModel, element.children[0])
-        });
-    AppMagic.UI.Pages.define("/controls/common/dialog/waitDialogBody.html", WaitDialogBodyView)
-})();
