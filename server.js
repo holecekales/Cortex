040fb88e85984d3f227f56f8277a244bf923d563
alesh <ales@outlook.com>
2017-09-16 10:07:21 -0700

    got all of the module depencnies sorted out

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const moment = require('moment');
+const path = require('path');
 var sPi = require('./motion');
 
 var app = express();
@@ -7,12 +11,11 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-
+// directories on the service
 app.use('/upload', express.static('upload'));
-
 app.use('/public/', express.static('public'));
 
-
+// APIs
 app.use('/api/motion', sPi);
 
 // catch all for the rest of the APIs
@@ -21,5 +24,5 @@ app.use('/api', function(req, res, next) {
 });
 
 app.listen(port, function () {
-  console.log('Listening on port ' + port);
+  console.log('Listening on port %d', port);
 });
